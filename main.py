7176a044e5befa624de563e1de31518b2862a149
Lynne Munini <muninilynne65@gmail.com>
2021-11-18 19:02:35 -0500

    Final Commit

--- main.py
+++ main.py
@@ -3,23 +3,36 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+global unlocked
 s = Service("/home/lynne/Programs/Development/chromedriver")
 driver = webdriver.Chrome(service=s)
 driver.get("https://orteil.dashnet.org/cookieclicker/")
 cookie = driver.find_element(By.XPATH, "//*[@id='bigCookie']")
-timeout = time.time() + 10  # 60*5 5 minutes from now
+timeout = time.time() + 30  # 60*5 5 minutes from now
 five_secs = time.time() + 5
-prices_list = []
+products = driver.find_elements(By.CSS_SELECTOR, "#products")
+product_ids = [product.get_attribute("id") for product in products]
 while True:
     cookie.click()
-    if time.time() > five_secs:
-        all_prices = driver.find_elements(By.CLASS_NAME, "price")
-        for price in all_prices:
-            #To get the price
-            price = price.get_attribute('innerHTML')
-            prices_list.append(price)
-            five_secs = time.time() + 5
-    print(prices_list)
+    if time.time() >= five_secs:
+        for each in product_ids:
+            unlocked_products = driver.find_elements(By.CSS_SELECTOR, ".product.unlocked.enabled")
+            # print(unlocked_products)
+            prices_list = [int(element.text.split()[1]) for element in unlocked_products]
+            cookies = driver.find_element(By.ID, "cookies").text.split()[0]
+            cookies_count = int(cookies)
+            # print(cookies)
+            # to get most expensive upgrade that is affordable
+            affordable_upgrade = [None]
+            for price in prices_list:
+                if price <= cookies_count:
+                    affordable_upgrade[0] = price
+            affordable_product = str(affordable_upgrade[0])
+            for product in unlocked_products:
+                unlocked = product.text
+                if affordable_product in unlocked:
+                    product.click()
+        five_secs = time.time() + 5
 
     if time.time() > timeout:
         break
